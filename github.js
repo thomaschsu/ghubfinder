b5fad7a0fa05dc63fa25bf27c4e8bcce1feeb50a
Tom Hsu <thomaschsu@gmail.com>
2019-12-13 12:32:39 -0500

    Added display repos functionality

--- github.js
+++ github.js
@@ -2,14 +2,19 @@ class GitHub {
     constructor() {
         this.client_id = '3d7c779cf023b7da0239';
         this.client_secret = 'eca5a252c6a4228e7adb587bf1bb5ef5d4575743';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }

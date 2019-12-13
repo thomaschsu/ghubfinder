7602238acfc8caee01affea06de7fc1dfeb4e91c
Tom Hsu <thomaschsu@gmail.com>
2019-12-12 19:32:23 -0500

    Added the display profile

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init Github
 const github = new GitHub;
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.querySelector('#searchUser');
@@ -17,10 +19,11 @@ searchUser.addEventListener('keyup', (e) => {
 
                 } else {
                     // Show profile
+                    ui.showProfile(data.profile);
                 }
             })
     } else {
         // Clear profile
-        
+
     }
 });
